b55d385493f090cc78535d228665cf94657263cf
Mohammad Ravand <m.ravand@clickhub.ir>
2023-10-14 22:56:05 +0330

    first inia

---
+++ resources/js/Pages/Socket/Show.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+
+<template>
+    <h2>show websocket</h2>
+</template>
+
---
+++ resources/js/Pages/Socket/create.vue
@@ -0,0 +1,26 @@
+<script setup>
+import {ref} from 'vue';
+
+const message = ref('');
+
+function sentMessage(){
+ // Assuming you've set up Laravel Echo properly
+ Echo.channel('chat') // replace 'chat' with your channel name
+        .whisper('message', {
+            content: message.value,
+        });
+    message.value = ''; // Clear the input after sending
+}
+
+</script>
+
+
+<template>
+    <h2>Create Message websocket</h2>
+    <hr>
+    <div>
+        <input v-model="message" />
+        <button @click="sentMessage" class="btn btn-blue-500 rounded-md px-5 py-2">send message </button>
+    </div>
+</template>
+
