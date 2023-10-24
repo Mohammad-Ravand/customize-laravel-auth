ebd3e38b9508deb3bb13f7e6f1588e7eafd39b3a
Mohammad Ravand <m.ravand@clickhub.ir>
2023-10-24 19:59:43 +0330

    create models Role,Access,RoleUser,AccessUser with -a by artisan make

---
+++ app/Http/Controllers/AccessUserController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AccessUser;
+use App\Http\Requests\StoreAccessUserRequest;
+use App\Http\Requests\UpdateAccessUserRequest;
+
+class AccessUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAccessUserRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AccessUser $accessUser)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AccessUser $accessUser)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAccessUserRequest $request, AccessUser $accessUser)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AccessUser $accessUser)
+    {
+        //
+    }
+}
